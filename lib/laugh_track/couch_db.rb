62801ea6b4ef2a2826e13cbf2a9a7967e2ae39a1
Pat Allan <pat@freelancing-gods.com>
2010-03-05 14:39:55 +1100

    Telling Throne not to auto-create the couch server. For some reason, couch is being stupid.

--- lib/laugh_track/couch_db.rb
+++ lib/laugh_track/couch_db.rb
@@ -2,6 +2,6 @@ module LaughTrack::CouchDb
   private
   
   def db
-    @db ||= Throne::Database.new 'http://127.0.0.1:5984/laughtrack'
+    @db ||= Throne::Database.new 'http://127.0.0.1:5984/laughtrack', false
   end
 end
