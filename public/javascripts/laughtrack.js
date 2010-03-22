9a2775b61fc2ac017abcad7e9733b53bd1fcdf48
benvolio <ben.webster@gmail.com>
2010-03-23 05:57:21 +1100

    cleaning up tables

---
+++ public/javascripts/laughtrack.js
@@ -0,0 +1,15 @@
+jQuery(function($) {
+  
+
+  // stripe tables
+  $('tr:odd').removeClass('even');
+  $('tr:even' ).addClass('even');
+
+  // highlight rows
+  $('tr').mouseenter(function() {
+    $(this).addClass('highlight');
+  }).mouseleave(function() {
+    $(this).removeClass('highlight');
+  });
+
+});
