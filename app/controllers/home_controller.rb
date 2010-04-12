f67e14070372b91567964dde674fa1ea484d2e99
nathanscott <nathan@inspire9.com.au>
2010-04-12 20:38:37 +1000

    performances cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
   def index
     @popular  = Show.popular.limited.available.still_showing
     @rated    = Show.rated.limited
-    @featured = Show.featured.random.first || Show.random.first
+    # @featured = Show.featured.random.first || Show.random.first
     @tonight  = Show.tonight.rated.popular.available.very_limited
-    @recent_tweets    = @rated.collect(&:random_tweet)
-    @tweet    = @featured.random_tweet
+    @recent_tweets    = @rated.collect(&:random_tweet) #FIXME:  Needs a recent tweets method on shows  >  CouchDB stuff
+    # @tweet    = @featured.random_tweet
     @calendar_date = Time.parse("#{params[:date]}-01") || Time.now
-    @performance_dates = Performance.find(:all, :conditions => ["happens_at BETWEEN ? AND ?", @calendar_date, @calendar_date+1.month]).group_by{|p| p.happens_at.to_date}
+    # @performance_dates = Performance.find(:all, :conditions => ["happens_at BETWEEN ? AND ?", @calendar_date, @calendar_date+1.month]).group_by{|p| p.happens_at.to_date}
   end
   
   def about
