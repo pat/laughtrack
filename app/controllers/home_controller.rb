a441f4a257552b80e8da5c96dbfc727b238bbb18
nathanscott <nathan@inspire9.com.au>
2010-04-12 20:40:44 +1000

    unbreak home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     @recent_tweets    = @rated.collect(&:random_tweet) #FIXME:  Needs a recent tweets method on shows  >  CouchDB stuff
     # @tweet    = @featured.random_tweet
     @calendar_date = Time.parse("#{params[:date]}-01") || Time.now
-    # @performance_dates = Performance.find(:all, :conditions => ["happens_at BETWEEN ? AND ?", @calendar_date, @calendar_date+1.month]).group_by{|p| p.happens_at.to_date}
+    @performance_dates = Performance.find(:all, :conditions => ["happens_at BETWEEN ? AND ?", @calendar_date, @calendar_date+1.month]).group_by{|p| p.happens_at.to_date}
   end
   
   def about
