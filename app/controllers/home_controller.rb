c38eaed86bf59329c846e50312a723ae4a5a65a3
Pat Allan <pat@freelancing-gods.com>
2010-04-12 21:29:41 +1000

    Adding proper latest-tweets collection.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,28 @@
 class HomeController < ApplicationController
+  include LaughTrack::CouchDb
+  
   def index
-    @popular  = Show.popular.limited.available.still_showing
-    @rated    = Show.rated.limited
-    # @featured = Show.featured.random.first || Show.random.first
-    @tonight  = Show.tonight.rated.popular.available.very_limited
-    @recent_tweets    = @rated.collect(&:random_tweet) #FIXME:  Needs a recent tweets method on shows  >  CouchDB stuff
-    # @tweet    = @featured.random_tweet
-    @calendar_date = Time.parse("#{params[:date]}-01") || Time.now
+    @popular           = Show.popular.limited.available.still_showing
+    @rated             = Show.rated.limited
+    @tonight           = Show.tonight.rated.popular.available.very_limited
+    @recent_tweets     = recent_tweets
+    @calendar_date     = Time.parse("#{params[:date]}-01") || Time.now
     @performance_dates = Performance.find(:all, :conditions => ["happens_at BETWEEN ? AND ?", @calendar_date, @calendar_date+1.month]).group_by{|p| p.happens_at.to_date}
   end
   
   def about
     @title = "About"
+  end
+  
+  private
+  
+  def recent_tweets
+    tweet_pointers.collect { |pointer| db.get pointer.id }
+  end
+  
+  def tweet_pointers
+    db.function "_design/laughtrack/_view/confirmed_by_time",
+      :limit      => 5,
+      :descending => true
   end
 end
