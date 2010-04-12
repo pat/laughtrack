60615a1c10a7c8097b3c3a45aef00b9f7e382021
nathanscott <nathan@inspire9.com.au>
2010-04-12 22:08:21 +1000

    sortable columns in performances

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,7 +1,8 @@
 class PerformancesController < ApplicationController
   def index
-    @performances = Performance.for(date).ordered.available.paginate(
-      :page => params[:page]
+    @date = date
+    @performances = Performance.for(date).ordered.available.find(:all, :include => [:show => [:act]], :order => "#{order} #{sort_mode}" ).paginate(
+        :page      => params[:page]
     )
   end
   
@@ -11,5 +12,31 @@ class PerformancesController < ApplicationController
     return Date.today if [:year, :month, :date].all? { |key| params[key].blank?}
     
     Date.new params[:year].to_i, params[:month].to_i, params[:date].to_i
+  end
+  
+  def sort_mode
+    case params[:order]
+    when 'asc'
+      :asc
+    when 'desc'
+      :desc
+    else
+      nil
+    end
+  end
+  
+  def order
+    case params[:sort_by]
+    when 'act'
+      'acts.name'
+    when 'show'
+      'shows.name'
+    when 'sold_out'
+      'shows.sold_out_percent'
+    when 'rating'
+      'shows.rating'
+    else
+      'happens_at'
+    end
   end
 end
