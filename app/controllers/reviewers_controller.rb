0c53939c16615b224302a5ee6eddf84ad11a8655
Pat Allan <pat@freelancing-gods.com>
2011-03-29 12:15:12 +1100

    We only want confirmed tweets.

--- app/controllers/reviewers_controller.rb
+++ app/controllers/reviewers_controller.rb
@@ -2,6 +2,7 @@ class ReviewersController < ApplicationController
   expose(:reviewers) {
     Reviewer.joins(:tweets).
       select('reviewers.*, COUNT(DISTINCT tweets.id) AS tweet_count').
+      where('tweets.confirmed = ?', true).
       group('reviewers.id, reviewers.username, reviewers.created_at, reviewers.updated_at').
       order('COUNT(DISTINCT tweets.id) DESC')
   }
