29071562bbafeaef9ced0e63521cc929d91b5588
Pat Allan <pat@freelancing-gods.com>
2012-03-24 16:08:53 +1100

    Cloudfront as a CDN.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Laughtrack::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "cf2012.laughtrack.com.au"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
