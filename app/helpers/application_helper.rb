e33c1f5b42dd6773d21f05553ca184c3bd42aceb
Elle Meredith <ellemeredith@gmail.com>
2012-03-24 21:58:33 +0700

    shows list sidebar styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
       content_tag(:p, msg)
     end
   end
+
+  def show_heading(show)
+    content_tag(:h4, show.heading_one) + content_tag(:p, show.heading_two)
+  end
 end
