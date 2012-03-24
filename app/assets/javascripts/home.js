27a275b12455e928571bc6f4abd8af9b6285e3ca
Pat Allan <pat@freelancing-gods.com>
2012-03-24 14:31:14 +1100

    Slower timeout for search, fixing up featured class.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,11 +5,16 @@ $(document).ready(function() {
   $('body.home .navbar-search').submit(function() {
     var form = $(this);
     jQuery.pjax({
-      url: form.attr('action') + '?' + form.serialize(),
-      container: '#shows'
+      url:       form.attr('action') + '?' + form.serialize(),
+      container: '#shows',
+      timeout:   2000
     });
     return false;
   });
 
-  $('#shows a').pjax('#main-content');
-})
+  $('#shows a').pjax('#main-content').live('click', function() {
+    var list_item = $(this).parent('li');
+    list_item.siblings().removeClass('featured');
+    list_item.addClass('featured');
+  });
+});
