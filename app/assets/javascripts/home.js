373a967e368bf92292be7c4e7fc7d97e1ee131f3
Pat Allan <pat@freelancing-gods.com>
2012-03-25 12:52:36 +1100

    Client-side search.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-  if (!jQuery.support.pjax)
-    return;
+  var updateShowsFromSearch = function(event) {
+    var query = $('body.home. .navbar-search input').val();
+    var regex = new RegExp(query, 'im')
 
-  $('body.home .navbar-search').submit(function() {
-    var form = $(this);
-    jQuery.pjax({
-      url:       form.attr('action') + '?' + form.serialize(),
-      container: '#shows',
-      timeout:   3000
+    $('#shows li').each(function() {
+      var li = $(this);
+      if (li.text().match(regex))
+        li.show();
+      else
+        li.hide();
     });
+  };
+
+  updateShowsFromSearch();
+  $('body.home. .navbar-search input').on('keyup', updateShowsFromSearch);
+  $('body.home .navbar-search').submit(function() {
     return false;
   });
 
+  if (!jQuery.support.pjax)
+    return;
 
   $('body.home #shows a').pjax({
     container: '#main',
