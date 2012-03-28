8a43486c2675c15cb189bb4d2421ddffc8b716d3
Pat Allan <pat@freelancing-gods.com>
2012-03-28 19:37:15 +1100

    Fixing up keyboard shortcuts

--- app/assets/javascripts/manage_tweets.js
+++ app/assets/javascripts/manage_tweets.js
@@ -1,45 +1,47 @@
-window.focusedRow = $('table tbody tr:first-child');
-window.focusedRow.addClass('active');
-
-$(document).on('keydown', function(event) {
-  if (window.focusedRow.length == 0) return;
-
-  switch(event.which) {
-    case 87:
-      toggleAndFocus('wait');
-      break;
-    case 73:
-      toggleAndFocus('ignore');
-      break;
-    case 75:
-      toggleAndFocus('keep');
-      break;
-    case 78:
-      toggleAndFocus('negative');
-      break;
-    case 80:
-      toggleAndFocus('positive');
-      break
-  }
-});
-
-toggleAndFocus = function(value) {
-  window.scrollTo(0, window.focusedRow.position().top - 250);
-  window.focusedRow.find('input[value=' + value + ']').click();
-  window.focusedRow.removeClass('active');
-
-  window.focusedRow = window.focusedRow.next();
+$(document).ready(function() {
+  window.focusedRow = $('table tbody tr:first-child');
   window.focusedRow.addClass('active');
-}
 
-$("select[name='tweet[show_id]']").chosen();
+  $(document).on('keydown', function(event) {
+    if (window.focusedRow.length == 0) return;
 
-$('.attach-tweet form').on('submit', function() {
-  form = $(this);
-  jQuery.post(form.attr('action'), form.serialize(), function(data) {
-    form.parent('div.attach-tweet').remove();
-    var counter = $('#tweet-counter');
-    counter.text((parseInt(counter.text()) - 1).toString());
+    switch(event.which) {
+      case 87:
+        toggleAndFocus('wait');
+        break;
+      case 73:
+        toggleAndFocus('ignore');
+        break;
+      case 75:
+        toggleAndFocus('keep');
+        break;
+      case 78:
+        toggleAndFocus('negative');
+        break;
+      case 80:
+        toggleAndFocus('positive');
+        break
+    }
   });
-  return false;
+
+  toggleAndFocus = function(value) {
+    window.scrollTo(0, window.focusedRow.position().top - 250);
+    window.focusedRow.find('input[value=' + value + ']').click();
+    window.focusedRow.removeClass('active');
+
+    window.focusedRow = window.focusedRow.next();
+    window.focusedRow.addClass('active');
+  }
+
+  $("select[name='tweet[show_id]']").chosen();
+
+  $('.attach-tweet form').on('submit', function() {
+    form = $(this);
+    jQuery.post(form.attr('action'), form.serialize(), function(data) {
+      form.parent('div.attach-tweet').remove();
+      var counter = $('#tweet-counter');
+      counter.text((parseInt(counter.text()) - 1).toString());
+    });
+    return false;
+  });
 });
